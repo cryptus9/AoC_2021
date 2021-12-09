23c4cb72c1cf232a209e9eda9c674b86d707dd2a
Jannes <jmuense@it-economics.de>
2021-12-09 14:28:36 +0100

    Solve day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,54 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun readSonarLog(): List<Int> {
+        val logList = mutableListOf<Int>();
+        readInput("day1-sonarLog").forEach { logList.add(it.toInt()) }
+        return logList;
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    val sonarLog = readSonarLog();
 
+    fun getSolutionOfPart1(logList: List<Int>): Int {
+        var previousLog: Int = -1
+        var logWasDeeperAmount = 0
+        for (currentLog in logList) {
+            if (previousLog != -1) {
+                if (currentLog > previousLog) {
+                    logWasDeeperAmount++
+                }
+            }
+            previousLog = currentLog;
+        }
+        return logWasDeeperAmount
+    }
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(2000 == sonarLog.size)
+    check(193 == sonarLog[0])
+    check(10983 == sonarLog[1999])
+    println("Solution of Part1 = " + getSolutionOfPart1(sonarLog))
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    fun getListTripleSum(list: List<Int>, startIndex: Int): Int {
+        return list.subList(startIndex, startIndex + 3).sum()
+    }
+
+
+    fun getSolutionOfPart2(input: List<Int>): Int {
+        var sumDidInceaseCounter = 0;
+        for (i in 0 until input.size) {
+            try {
+                val didSumIncrease = getListTripleSum(input, i) < getListTripleSum(input, i + 1)
+                if (didSumIncrease) {
+                    sumDidInceaseCounter++
+                }
+            }catch (e: IndexOutOfBoundsException){
+                break
+            }
+        }
+        return sumDidInceaseCounter;
+    }
+
+    check(6 == getListTripleSum(listOf(1, 2, 3, 4, 5, 6), 0))
+    check(15 == getListTripleSum(listOf(1, 2, 3, 4, 5, 6), 3))
+    println("Solution of Part2 = " + getSolutionOfPart2(sonarLog))
+
 }
